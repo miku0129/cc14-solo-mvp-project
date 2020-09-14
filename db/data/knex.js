d58e248f24100656c6ae7f3eb9c415d942569b27
miku <svartkatt39@gmail.com>
2020-09-14 19:43:04 +0900

    done seeding

--- db/data/knex.js
+++ db/data/knex.js
@@ -1,10 +1,16 @@
 console.log("hello. in knex.js");
 
 const knex = require("knex");
-const config = require("../../db/data/knexfile");
+console.log("debug1,hello. in knex.js");
+
+// const config = require("../../db/data/knexfile");
+const config = require("../../knexfile");
+console.log("debug2,hello. in knex.js");
+
 const db = knex(
   process.env.NODE_ENV === "production" ? config.production : config.development
 );
+console.log("debug3,hello. in knex.js");
 
 // const environment = process.env.NODE_ENV || "development";
 // const config = require("../knexfile")[environment];
--- db/migrations/20200914165819_restaurants.js
+++ db/migrations/20200914165819_restaurants.js
@@ -1,8 +1,17 @@
-
-exports.up = function(knex) {
-  
+exports.up = function (knex, Promise) {
+  return knex.schema.createTable("restaurants", function (table) {
+    table.increments();
+    table.string("name").notNullable();
+    table.string("feature").notNullable();
+    table.string("tell").notNullable();
+    table.string("business_hours1").notNullable();
+    table.string("business_hours2").notNullable();
+    table.string("regular_holiday").notNullable();
+    table.string("place").notNullable();
+    table.string("url").notNullable();
+  });
 };
 
-exports.down = function(knex) {
-  
+exports.down = function (knex, Promise) {
+  return knex.schema.dropTable("restaurants");
 };
