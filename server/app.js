8402c901812d52ebd29cd10662474a7d1597cd00
miku <svartkatt39@gmail.com>
2020-09-15 09:28:18 +0900

    add static, build to app.js

--- server/app.js
+++ server/app.js
@@ -1,18 +1,18 @@
 console.log("hello I'm app.js");
 const express = require("express");
-// const path = require("path");
+const path = require("path");
 // const db = require("../knex.js");
 const db = require("../db/data/restaurants.json");
 
 const setupServer = () => {
-  console.log("hello. in setupServer");
+  console.log("hello. in setupServer!!");
   const app = express();
 
   app.use(express.json());
 
   //public folder
-  //app.use(express.static(path.resolve(__dirname, "..", "buid")));
-  app.use(express.static("public"));
+  app.use(express.static(path.resolve(__dirname, "..", "buid")));
+  // app.use(express.static("public"));
 
   app.get("/api/restaurants", (req, res) => {
     res.send(db);
